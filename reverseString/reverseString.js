9d5af07b24afc06120e745a9bce976dce0117c5a
abaker93 <anna.baker.design@gmail.com>
2020-09-14 17:10:39 -0400

    reverseString exercise

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let newString = "";
 
+	for (i=string.length-1; i>=0; i--) {
+		let char = string.charAt(i);
+		newString = newString.concat(char);
+	}
+
+	return newString;
 }
 
 module.exports = reverseString
