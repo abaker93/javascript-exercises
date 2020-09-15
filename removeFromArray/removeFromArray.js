9eb63eda03e2ea23c06602b4d4458ff313f0af57
abaker93 <anna.baker.design@gmail.com>
2020-09-15 10:50:04 -0400

    removeFromArray exercise

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    const array = args[0];
+    let newArray = [];
 
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push(item);
+        }
+    });
+
+    return newArray;
 }
 
 module.exports = removeFromArray
