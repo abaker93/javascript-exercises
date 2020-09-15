703993a6349938addb9d7cdbfd762e3aa31bb9c0
abaker93 <anna.baker.design@gmail.com>
2020-09-15 11:06:41 -0400

    sumAll exercise

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') return 'ERROR';
+	if (num1 < 0 || num2 < 0) return 'ERROR';
+	const numbers = [];
+	let sum = 0;
 
+	if (num1 < num2) {
+		for (i=num1; i<num2+1; i++) numbers.push(i);
+	} else {
+		for (i=num2; i<num1+1; i++) numbers.push(i);
+	}
+
+    numbers.forEach((number) => { sum = sum + number; })
+    
+    return sum;
 }
 
 module.exports = sumAll
