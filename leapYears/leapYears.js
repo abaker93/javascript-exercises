688804cadbdaa8c4d49c4a591a65c84c0cbfffee
abaker93 <anna.baker.design@gmail.com>
2020-09-15 11:19:17 -0400

    leapYear exercise

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,22 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // const divBy4 = year % 4;
+    // const divBy100 = year % 100;
+    // const divBy400 = year % 400;
+    // if (divBy4 === 0) {
+    //     if (divBy100 === 0) {
+    //         if (divBy400 === 0) {
+    //             return true;
+    //         } else {
+    //             return false;
+    //         }
+    //     } else {
+    //         return true;
+    //     }
+    // } else {
+    //     return false;
+    // }
 
+    return year % 4 === 0 && ( year % 100 !== 0 || year % 400 == 0 )
 }
 
 module.exports = leapYears
