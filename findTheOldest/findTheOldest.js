292135fb6a7c6fb7410e841e9fee614c48416f97
abaker93 <anna.baker.design@gmail.com>
2020-09-17 10:51:45 -0400

    findTheOldest exercise

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-let findTheOldest = function() {
+let findTheOldest = function(arr) {
+    const currYear = new Date().getFullYear();
+	let people = [];
+    arr.forEach(person => {
+		if (!person.yearOfDeath) person.yearOfDeath = currYear;
+		let age = person.yearOfDeath - person.yearOfBirth;
+		let obj = {
+			"name": person.name,
+			"yearOfBirth": person.yearOfBirth,
+			"yearOfDeath": person.yearOfDeath,
+			"age": age
+		}
+		people.push(obj);
+	});
 
+	let oldestPerson = people[0];
+	people.forEach(person => {
+		if (person.age > oldestPerson.age) oldestPerson = person;
+	})
+
+	return oldestPerson;
 }
 
 module.exports = findTheOldest
