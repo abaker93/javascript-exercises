e37c0ddc294d28bfd33ce3182cf4e6936f76af75
abaker93 <anna.baker.design@gmail.com>
2020-09-16 22:38:13 -0400

    palindromes exercise

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function() {
+const palindromes = function(word) {
+    let string = word.replace(/[.,\/#!$%\^&\*;:{}=\-	_`~()]/g,"");
+	string = string.replace(/\s/g, '');
+	string = string.toLowerCase();
+	let reversedString = '';
 
+	for (i=string.length-1; i>=0; i--) {
+		reversedString = reversedString.concat(string.charAt(i));
+	}
+
+	if (string === reversedString) return true
+	else return false;
 }
 
 module.exports = palindromes
