768c001c5a76b501c787e07ac1c6cc6e2c7304d3
abaker93 <anna.baker.design@gmail.com>
2020-09-16 22:07:52 -0400

    calculator exercise

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,48 @@
-function add () {
-	
+function add (x, y) {
+	return x + y;
 }
 
-function subtract () {
-	
+function subtract (x, y) {
+	return x - y
 }
 
-function sum () {
-	
+function sum (...args) {
+	const array = args[0];
+	let sum = 0;
+
+	for (i=0; i<array.length; i++) {
+		sum = sum + array[i];
+	}
+
+	return sum;
 }
 
-function multiply () {
+function multiply (...args) {
+	const array = args[0];
+	let product = array[0];
 	
+	for (i=1; i<array.length; i++) {
+		product = product * array[i];
+	}
+	
+	return product;
 }
 
-function power() {
-	
+function power(x, y) {
+	return Math.pow(x, y);
 }
 
-function factorial() {
-	
+function factorial(x) {
+	let array = [];
+	let factorial = x;
+
+	if (x === 0) return 1;
+
+	for (i=1; i<x; i++) array.push(i);
+
+	for (i=0; i<array.length; i++) factorial = factorial * array[i];
+
+	return factorial;
 }
 
 module.exports = {
