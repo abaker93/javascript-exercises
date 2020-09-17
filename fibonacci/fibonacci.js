b7711bdbc53e6c665ceb5796735c15bf9892210f
abaker93 <anna.baker.design@gmail.com>
2020-09-17 11:16:22 -0400

    fibonacci exercise

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(int) {
+    let num = 0;
+	if (typeof int !== 'number') {
+		num = parseInt(int, 10);
+	} else {
+		num = int
+	}
+	if (num <= 0) return 'OOPS';
+	let x = 0, y = 1, temp, arr = [];
+	arr.push(y);
 
+	for (i=1; i<num; i++) {
+		temp = y;
+		y = x + y;
+		x = temp;
+		arr.push(y);
+	}
+
+	return arr[num-1];
 }
 
 module.exports = fibonacci
